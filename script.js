451ceba2deacea01eb8c55e72253c4ab7b7403ec
yukthas02-oss <yukthas02@gmail.com>
2026-07-13 21:47:58 +0530

    Add interactive evidence notebook

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ function startInvestigation(){
     gameArea.style.display = "block";
   
 }
+const knifeButton = document.getElementById("knifeBtn");
+const notebook = document.getElementById("notebook");
+knifeButton.addEventListener("click", inspectKnife);
+
+function inspectKnife() {
+
+    notebook.textContent =
+        "Evidence Collected\n\n.... Bloody Knife\nThe fingerprints have been wiped clean.";
+
+}
+const phoneButton = document.getElementById("phoneBtn");
+phoneButton.addEventListener("click",inspectPhone);
+
+function inspectPhone(){
+    notebook.textContent = "Evidence Collected\n\n....Victim's Phone\nAll the Data has been cleared.";
+}
+const shoeButton = document.getElementById("shoeBtn");
+shoeButton.addEventListener("click",inspectShoe);
+
+function inspectShoe() {
+    notebook.textContent = "Evidence Collected\n\n....Victim'm Shoe\nNo fingerprints found"
+}
